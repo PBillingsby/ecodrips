3e015213e4b0427eb8dc206d910ce0a1a11db69d
PBillingsby <philbillingsby@gmail.com>
2022-03-04 12:59:01 -0800

    Fix repsponsiveness of landing button

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -15,14 +15,14 @@ export default function LandingPage() {
       <Box color='teal.100'>
         <Heading size='lg'>The Drip</Heading>
         <Text py={2}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
-        <Center my={4}>
-          <Button onClick={onOpen} size='lg' w='20rem' color='white' p={8} bgGradient='linear(to-r, #01281a, #316e46, #3c5e48)' _hover={{
-            background: "teal.100",
-            color: "#01281a",
-          }}>Start Dripping</Button>
-          <FormModal isOpen={isOpen} onClose={onClose} />
-        </Center>
       </Box>
+      <Center my={[0, 4]}>
+        <Button onClick={onOpen} w={['10rem', '20rem']} color='#eee' p={[0, 8]} bgGradient='linear(to-r, #01281a, #316e46, #3c5e48)' _hover={{
+          background: "teal.100",
+          color: "#01281a",
+        }}>Start Dripping</Button>
+        <FormModal isOpen={isOpen} onClose={onClose} />
+      </Center>
     </VStack>
   )
 }
