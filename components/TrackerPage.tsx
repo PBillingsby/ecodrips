61e865ecbd9999fd36463df10b3c38dad02e99bb
PBillingsby <philbillingsby@gmail.com>
2022-03-03 19:50:45 -0800

    Add Radicle subgraph query to TrackerPage

--- components/TrackerPage.tsx
+++ components/TrackerPage.tsx
@@ -1,6 +1,31 @@
 import { Heading } from '@chakra-ui/react';
+import { createClient } from 'urql';
+import { useEffect } from 'react';
 
+const query = `
+  query {
+    tokens {
+      id
+      tokenID
+      contentURI
+      metadataURI
+    }
+  }
+`
+const API_KEY = `https://gateway.thegraph.com/api/${process.env.NEXT_PUBLIC_RADICLE_API_KEY}/subgraphs/id/8rp4W6dGKdfYTf3So7xFpdd58gCaNbKwmm6Sketd4XvX`;
+
+const client = createClient({
+  url: API_KEY
+})
 export default function TrackerPage() {
+  useEffect(() => {
+    fetchData();
+  }, [])
+  async function fetchData() {
+    const response = await client.query(query).toPromise();
+    // Handle data returned from query
+    console.log(response);
+  }
   return (
     <Heading size='lg' p={2} color='teal.100'>
       Drip Tracker
